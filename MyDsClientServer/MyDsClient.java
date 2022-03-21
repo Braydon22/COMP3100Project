6a2401f7fea706ba763a15869392d41dd1b38468
Braydon <braydon.chi@students.mq.edu.au>
2022-03-21 11:25:21 +1100

    able to get and schedule all jobs

--- MyDsClientServer/MyDsClient.java
+++ MyDsClientServer/MyDsClient.java
@@ -13,10 +13,21 @@ String serverMsg = "", cmd = "", helo="HELO\n", auth="AUTH braydon\n";
 os.write(helo.getBytes("US-ASCII"));
 os.write(auth.getBytes("US-ASCII"));
 
-os.write(("REDY\n").getBytes("US-ASCII"));
-
+// get and schedule jobs 
+//int serverId = 0;
+for(int i = 0; i < 11; i++) {
 serverMsg = in.readLine();
 System.out.println("Server: "+serverMsg);
+
+os.write(("REDY\n").getBytes("US-ASCII"));
+//os.write(("GETS Capable 3 700 8000\n").getBytes("US-ASCII"));
+//os.write(("OK\n").getBytes("US-ASCII"));  
+os.write(("SCHD "+i+" joon 1\n").getBytes("US-ASCII"));
+
+//if(serverMsg.length() > 4 && serverMsg.substring(0, 4) == "DATA") {
+//}
+
+}
 
 os.write(("QUIT\n").getBytes("US-ASCII"));
 
