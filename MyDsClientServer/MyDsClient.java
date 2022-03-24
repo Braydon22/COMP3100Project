6aa6e759da5662352afa5865933162f55bfeb1ec
Braydon <braydon.chi@students.mq.edu.au>
2022-03-24 20:42:28 +1100

    able to handle server information and find the server with largest cores

--- MyDsClientServer/MyDsClient.java
+++ MyDsClientServer/MyDsClient.java
@@ -1,8 +1,11 @@
 import java.io.*;  
-import java.net.*;  
+import java.net.*;
+  
+
 public class MyDsClient {  
 public static void main(String[] args) {  
-try{      
+try{
+      
 Socket s=new Socket("localhost", 50000);  
 DataOutputStream os=new DataOutputStream(s.getOutputStream());
 BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -14,18 +17,67 @@ os.write(helo.getBytes("US-ASCII"));
 os.write(auth.getBytes("US-ASCII"));
 
 // get and schedule jobs 
-//int serverId = 0;
-for(int i = 0; i < 11; i++) {
+
+int jobId = 0;
+
+while(!serverMsg.equals("NONE")) {
 serverMsg = in.readLine();
-System.out.println("Server: "+serverMsg);
+//System.out.println("Server: "+serverMsg);
+String[] serverMsgVals = serverMsg.split("\\s+");
+//System.out.println("Server: "+serverMsgVals[0]);
+ 
 
+ServerInfo largestServer = new ServerInfo("N/A", 0, "N/A", 0, 0, 0, 0);
+
+if(serverMsgVals[0].equals("DATA")){
+
+String serverInfo; 
+
+for(int i = 0; i < Integer.parseInt(serverMsgVals[1]); i++) {
+serverInfo = in.readLine();
+String[] serverInfoVals = serverInfo.split("\\s+");
+
+ServerInfo sInfo = new ServerInfo(
+  serverInfoVals[0], 
+  Integer.parseInt(serverInfoVals[1]), 
+  serverInfoVals[2], 
+  Integer.parseInt(serverInfoVals[3]), 
+  Integer.parseInt(serverInfoVals[4]), 
+  Integer.parseInt(serverInfoVals[5]), 
+  Integer.parseInt(serverInfoVals[6])
+ );
+ 
+ sInfo.printServerInfo();
+
+ if(sInfo.cores > largestServer.cores) {
+   largestServer = sInfo;
+ }
+ 
+}
+
+System.out.println("largest server type: " + largestServer.type + " " + largestServer.id);
+}
+
+
+if(serverMsg.equals("OK")){
 os.write(("REDY\n").getBytes("US-ASCII"));
-//os.write(("GETS Capable 3 700 8000\n").getBytes("US-ASCII"));
-//os.write(("OK\n").getBytes("US-ASCII"));  
-os.write(("SCHD "+i+" joon 1\n").getBytes("US-ASCII"));
 
-//if(serverMsg.length() > 4 && serverMsg.substring(0, 4) == "DATA") {
-//}
+os.write(("GETS All\n").getBytes("US-ASCII"));
+
+os.write(("OK\n").getBytes("US-ASCII"));
+os.write(("OK\n").getBytes("US-ASCII"));
+
+
+os.write(("SCHD "+jobId+" joon 1\n").getBytes("US-ASCII"));
+jobId++;
+}
+
+
+
+
+if(serverMsgVals[0].equals("JCPL")){
+os.write(("REDY\n").getBytes("US-ASCII"));
+}
 
 }
 
@@ -38,3 +90,36 @@ s.close();
 }catch(Exception e){System.out.println(e);}  
 }  
 }  
+
+
+class ServerInfo {  
+
+String type;
+int id;
+String state;
+int curStartTime;
+int cores;
+int memory;
+int disk;
+
+public ServerInfo(String type, int id, String state, int curStartTime, int cores, int memory, int disk){
+  this.type = type;
+  this.id = id;
+  this.state = state;
+  this.curStartTime = curStartTime;
+  this.cores = cores;
+  this.memory = memory;
+  this.disk = disk;
+}
+
+public void printServerInfo(){
+System.out.println(
+"Server name: " + type + " " +
+"Server id: " + id + " " +
+"Server cores: " + cores + " " +
+"Server memory: " + memory + " " +
+"Server disk: " + disk 
+);
+}
+
+}
