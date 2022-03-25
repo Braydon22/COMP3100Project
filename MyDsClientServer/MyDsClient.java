2980354177e57da37ed378c8e6f7096f07413222
Braydon <braydon.chi@students.mq.edu.au>
2022-03-25 22:44:42 +1100

    able to retrieve largest server and schedule jobs using LRR, only works for config files 01 to 03

--- MyDsClientServer/MyDsClient.java
+++ MyDsClientServer/MyDsClient.java
@@ -1,6 +1,12 @@
 import java.io.*;  
 import java.net.*;
-  
+import javax.xml.parsers.DocumentBuilderFactory;  
+import javax.xml.parsers.DocumentBuilder;  
+import org.w3c.dom.Document;  
+import org.w3c.dom.NodeList;  
+import org.w3c.dom.Node;  
+import org.w3c.dom.Element;  
+import java.io.File; 
 
 public class MyDsClient {  
 public static void main(String[] args) {  
@@ -10,32 +16,52 @@ Socket s=new Socket("localhost", 50000);
 DataOutputStream os=new DataOutputStream(s.getOutputStream());
 BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-String serverMsg = "", cmd = "", helo="HELO\n", auth="AUTH braydon\n";
+// default largest
+ServerInfo largestServer = new ServerInfo("N/A", 0, "N/A", 0, 0, 0);
+int nextLargestServerIdx = 1;
+
+// retrieve largest server xml file and find largest server
+File dsXmlFile = new File("ds-system.xml");
+DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+Document doc = docBuilder.parse(dsXmlFile);
+
+NodeList dsNodeList = doc.getElementsByTagName("server");
+
+for(int i = 0; i < dsNodeList.getLength(); i++){
+int serverCores = Integer.parseInt(dsNodeList.item(i).getAttributes().getNamedItem("cores").getNodeValue());
+if(largestServer.cores < serverCores){
+String serverType = dsNodeList.item(i).getAttributes().getNamedItem("type").getNodeValue();
+int serverMemory = Integer.parseInt(dsNodeList.item(i).getAttributes().getNamedItem("memory").getNodeValue());
+int serverDisk = Integer.parseInt(dsNodeList.item(i).getAttributes().getNamedItem("disk").getNodeValue());
+largestServer = new ServerInfo(serverType, 0, "in-active", serverCores, serverMemory, serverDisk);
+}
+}
+
+String serverMsg = "", helo="HELO\n", auth="AUTH braydon\n";
 
 // establish connection
 os.write(helo.getBytes("US-ASCII"));
 os.write(auth.getBytes("US-ASCII"));
 
 // get and schedule jobs 
-
-int jobId = 0;
-
 while(!serverMsg.equals("NONE")) {
 serverMsg = in.readLine();
 //System.out.println("Server: "+serverMsg);
-String[] serverMsgVals = serverMsg.split("\\s+");
+String[] serverMsgVals = serverMsg.split(" ");
 //System.out.println("Server: "+serverMsgVals[0]);
  
-
-ServerInfo largestServer = new ServerInfo("N/A", 0, "N/A", 0, 0, 0, 0);
 
 if(serverMsgVals[0].equals("DATA")){
 
 String serverInfo; 
+int numberOfLargestServer = Integer.parseInt(serverMsgVals[1]);
 
-for(int i = 0; i < Integer.parseInt(serverMsgVals[1]); i++) {
+for(int i = 0; i < numberOfLargestServer; i++) {
 serverInfo = in.readLine();
-String[] serverInfoVals = serverInfo.split("\\s+");
+String[] serverInfoVals = serverInfo.split(" ");
+
+if(nextLargestServerIdx == i){
 
 ServerInfo sInfo = new ServerInfo(
   serverInfoVals[0], 
@@ -43,36 +69,40 @@ ServerInfo sInfo = new ServerInfo(
   serverInfoVals[2], 
   Integer.parseInt(serverInfoVals[3]), 
   Integer.parseInt(serverInfoVals[4]), 
-  Integer.parseInt(serverInfoVals[5]), 
-  Integer.parseInt(serverInfoVals[6])
+  Integer.parseInt(serverInfoVals[5])
  );
  
- sInfo.printServerInfo();
-
- if(sInfo.cores > largestServer.cores) {
-   largestServer = sInfo;
- }
+ largestServer = sInfo;
+ nextLargestServerIdx++;
  
+ break;
+}
+ 
+ //sInfo.printServerInfo();
 }
 
-System.out.println("largest server type: " + largestServer.type + " " + largestServer.id);
+if(nextLargestServerIdx == numberOfLargestServer){
+nextLargestServerIdx = 0;
+}
+
 }
 
 
 if(serverMsg.equals("OK")){
 os.write(("REDY\n").getBytes("US-ASCII"));
 
-os.write(("GETS All\n").getBytes("US-ASCII"));
+os.write(("GETS Type "+largestServer.type+"\n").getBytes("US-ASCII"));
 
 os.write(("OK\n").getBytes("US-ASCII"));
 os.write(("OK\n").getBytes("US-ASCII"));
 
 
-os.write(("SCHD "+jobId+" joon 1\n").getBytes("US-ASCII"));
-jobId++;
 }
 
-
+if(serverMsgVals[0].equals("JOBN")) {
+os.write(("SCHD "+serverMsgVals[2]+" "+largestServer.type+" "+largestServer.id+"\n").getBytes("US-ASCII"));
+System.out.println("SCHD "+serverMsgVals[2]+" "+largestServer.type+" "+largestServer.id);
+}
 
 
 if(serverMsgVals[0].equals("JCPL")){
@@ -97,16 +127,14 @@ class ServerInfo {
 String type;
 int id;
 String state;
-int curStartTime;
 int cores;
 int memory;
 int disk;
 
-public ServerInfo(String type, int id, String state, int curStartTime, int cores, int memory, int disk){
+public ServerInfo(String type, int id, String state, int cores, int memory, int disk){
   this.type = type;
   this.id = id;
   this.state = state;
-  this.curStartTime = curStartTime;
   this.cores = cores;
   this.memory = memory;
   this.disk = disk;
