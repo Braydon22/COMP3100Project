cb2f703777fd49e9e253f7629a0303c4c18f6335
Braydon <braydon.chi@students.mq.edu.au>
2022-03-28 14:57:24 +1100

    fixed an error: don't need to execute the program twice when changing confi files

--- MyDsClientServer/MyDsClient.java
+++ MyDsClientServer/MyDsClient.java
@@ -20,34 +20,72 @@ BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
 ServerInfo largestServer = new ServerInfo("N/A", 0, "N/A", 0, 0, 0);
 int nextLargestServerIdx = 1;
 
-// retrieve largest server xml file and find largest server
-File dsXmlFile = new File("ds-system.xml");
-DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-Document doc = docBuilder.parse(dsXmlFile);
-
-NodeList dsNodeList = doc.getElementsByTagName("server");
-
-for(int i = 0; i < dsNodeList.getLength(); i++){
-int serverCores = Integer.parseInt(dsNodeList.item(i).getAttributes().getNamedItem("cores").getNodeValue());
-if(largestServer.cores < serverCores){
-String serverType = dsNodeList.item(i).getAttributes().getNamedItem("type").getNodeValue();
-int serverMemory = Integer.parseInt(dsNodeList.item(i).getAttributes().getNamedItem("memory").getNodeValue());
-int serverDisk = Integer.parseInt(dsNodeList.item(i).getAttributes().getNamedItem("disk").getNodeValue());
-largestServer = new ServerInfo(serverType, 0, "in-active", serverCores, serverMemory, serverDisk);
-}
-}
-
 String serverMsg = "", helo="HELO\n", auth="AUTH braydon\n";
 
 // establish connection
 os.write(helo.getBytes("US-ASCII"));
 os.write(auth.getBytes("US-ASCII"));
 
+
+
+
+// get largest server type
+serverMsg = in.readLine();
+if(serverMsg.equals("OK")){
+os.write(("REDY\n").getBytes("US-ASCII"));
+
+os.write(("GETS All\n").getBytes("US-ASCII"));
+
+serverMsg = in.readLine();
+while(!serverMsg.split(" ")[0].equals("DATA")) {
+serverMsg = in.readLine();
+String[] curServerMsgVals = serverMsg.split(" ");
+
+System.out.println(serverMsg);
+}
+
+// loop through data to get largest server type
+os.write(("OK\n").getBytes("US-ASCII"));
+System.out.println("OK");
+String[] dataVals = serverMsg.split(" ");
+if(dataVals[0].equals("DATA")){
+int numberOfServers = Integer.parseInt(dataVals[1]);
+
+for(int i = 0; i < numberOfServers; i++) {
+String serverInfo = in.readLine();
+String[] serverInfoVals = serverInfo.split(" ");
+
+int serverCores = Integer.parseInt(serverInfoVals[4]);
+if(largestServer.cores < serverCores){
+String serverType = serverInfoVals[0];
+int serverId = Integer.parseInt(serverInfoVals[1]);
+String serverState = serverInfoVals[2];
+int serverMemory = Integer.parseInt(serverInfoVals[5]);
+int serverDisk = Integer.parseInt(serverInfoVals[6]);
+
+largestServer = new ServerInfo(serverType, serverId, serverState, serverCores, serverMemory, serverDisk);
+System.out.println(largestServer.type);
+}
+
+}
+
+}
+
+os.write(("OK\n").getBytes("US-ASCII"));
+System.out.println("OK");
+}
+
+serverMsg = in.readLine();
+if(serverMsg.equals(".")){
+os.write(("SCHD 0 "+largestServer.type+" "+largestServer.id+"\n").getBytes("US-ASCII"));
+}
+
+
+
+
 // get and schedule jobs 
 while(!serverMsg.equals("NONE")) {
 serverMsg = in.readLine();
-//System.out.println("Server: "+serverMsg);
 String[] serverMsgVals = serverMsg.split(" ");
 //System.out.println("Server: "+serverMsgVals[0]);
  
@@ -101,14 +139,19 @@ os.write(("OK\n").getBytes("US-ASCII"));
 
 if(serverMsgVals[0].equals("JOBN")) {
 os.write(("SCHD "+serverMsgVals[2]+" "+largestServer.type+" "+largestServer.id+"\n").getBytes("US-ASCII"));
-System.out.println("SCHD "+serverMsgVals[2]+" "+largestServer.type+" "+largestServer.id);
+//System.out.println("SCHD "+serverMsgVals[2]+" "+largestServer.type+" "+largestServer.id);
 }
 
 
 if(serverMsgVals[0].equals("JCPL")){
 os.write(("REDY\n").getBytes("US-ASCII"));
+
+if(serverMsgVals[0].equals("ERR:") || serverMsg.equals("")){
+break;
+}
 }
 
+System.out.println("Server: "+serverMsg);
 }
 
 os.write(("QUIT\n").getBytes("US-ASCII"));
